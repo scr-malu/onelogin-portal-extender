81f5940954549d1fe164b324952bbafd8a6306bf
Kosuke Osada <49811251+scr-malu@users.noreply.github.com>
2025-01-08 15:08:18 +0900

    正常動作するようMutationObserverで監視するよう修正

--- portal-01.js
+++ portal-01.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName("tab-item-content")[1].click();
+const observer = new MutationObserver((mutationsList, observer) => {
+    const tabs = document.getElementsByClassName("tab-item-content");
+    if (tabs[2]) {
+      tabs[2].click(); // [1] のタブをクリック
+      observer.disconnect(); // 監視を停止
+    }
+  });
+  
+  // 監視を開始（body要素の変更を監視）
+  observer.observe(document.body, { childList: true, subtree: true });
